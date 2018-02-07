b3cecd989d3c39fc6a28d0bdfe04a38ac0417006
zyra capili <zcapili@cambridge.org>
2018-02-07 16:04:10 +0800

    update commands

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -2,10 +2,10 @@
 # Download Conf Repo
 
 class profile::conf{
-    file {'/etc/puppetlabs/code/modules/tng_conf/':
-		ensure => 'absent',
-		force => true,
-	}~>
+#    file {'/etc/puppetlabs/code/modules/tng_conf/':
+#		ensure => 'absent',
+#		force => true,
+#	}~>
 	exec {'download config repo':
         command => '/usr/bin/git clone https://github.com/releaseph/test-anz.git /etc/puppetlabs/code/modules/tng_conf/',
 		refreshonly => 'true',
