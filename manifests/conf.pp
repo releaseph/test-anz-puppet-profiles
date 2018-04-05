452bf3b09e093f4a603d113755cb0c50ce9e37c8
Zyra Angeline Capili <zcapili@cambridge.org>
2018-04-05 11:48:43 +0800

    return refresh only

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -8,7 +8,7 @@ class profile::conf{
 	}~>
 	exec {'download config repo for Staging':
         command => '/usr/bin/git clone -b test https://github.com/releaseph/test-anz.git /etc/puppetlabs/code/modules/tng_conf/files/',
-		#refreshonly => 'true',
+		refreshonly => 'true',
     }~>
 	file {'/etc/puppetlabs/code/modules/tng_conf_live/':
 		ensure => 'absent',
@@ -16,6 +16,6 @@ class profile::conf{
 	}~>
 	exec {'download config repo for LIVE':
         command => '/usr/bin/git clone -b production https://github.com/releaseph/test-anz.git /etc/puppetlabs/code/modules/tng_conf_live/files/',
-		#refreshonly => 'true',
+		refreshonly => 'true',
     }
 }
